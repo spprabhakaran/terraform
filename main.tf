b664f4c7577425c9f492129bdefc265a26c312dc
spprabhakaran <157973196+spprabhakaran@users.noreply.github.com>
2024-07-08 12:37:10 -0700

    edited

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 terraform {
+  required_version = ">= 1.0.0"
+
   required_providers {
-    aws {
-        source = "hashicorp/aws"
-        version = "5.57.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
   }
-}
 
 provider "aws" {
     region = "us-east-1"
